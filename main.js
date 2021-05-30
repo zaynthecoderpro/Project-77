cf9f8f3103ca84eb38e66cd74eb1267878322606
zaynthecoderpro <85072791+zaynthecoderpro@users.noreply.github.com>
2021-05-30 12:24:36 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("my first console project")
+var name1 = "tim"
+var name2 = "cook"
+var fullname = name1.concat(name2);
+console.log(fullname);
+var a = 123
+var b = 877
+var add = a + b;
+console.log(add); 
+var sub = b - a;
+console.log(sub);
+var mult = a * b;
+console.log(mult);
+var div = b / a;
+console.log(div);
+
